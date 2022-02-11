91d4a7136c071bb64004c12ec7aa937b79acd6d8
fazlibalkan <fazlibalkan@outlook.com>
2022-02-11 22:26:12 +0300

    share component added

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,7 +1,12 @@
 import "./feed.css"
+import Share from "../share/Share.jsx"
 
 export default function feed() {
   return (
-    <div className="feed">feed</div>
+    <div className="feed">
+        <div className="feedWrapper">
+            <Share/>
+        </div>
+    </div>
   )
 }
