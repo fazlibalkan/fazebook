f3c90a401f4719c59547c349be7ff766d830a02e
fazlibalkan <fazlibalkan@outlook.com>
2022-02-12 20:46:26 +0300

    rightbar added

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,10 +1,30 @@
 import "./rightbar.css"
-import {MoreVert} from "@mui/icons-material"
+import {} from "@mui/icons-material"
 
 export default function Rightbar() {
   return (
     <div className="rightbar">
-        Rightbar
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImageContainer">
+              <img src="assets/person/2.jpeg" alt="" className="rightbarProfileImage" />
+              <span className="rightbarOnline"> </span>
+            </div>
+            <span className="rightbarUsername">Ali Öztürk</span>
+          </li>
+          
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImageContainer">
+              <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImage" />
+              <span className="rightbarOnline"> </span>
+            </div>
+            <span className="rightbarUsername">Ceren Yaman</span>
+          </li>
+
+        </ul>
     </div>
   )
 }
