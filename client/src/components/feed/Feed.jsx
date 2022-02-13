8a9091e9daf9804aebc32d0fc0433f8de4dcf08c
fazlibalkan <fazlibalkan@outlook.com>
2022-02-13 13:54:53 +0300

    post functionality added

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,17 +1,17 @@
 import "./feed.css"
 import Share from "../share/Share.jsx"
 import Post from "../post/Post.jsx"
+import {Posts} from "../../dummyData"
 
 export default function feed() {
   return (
     <div className="feed">
         <div className="feedWrapper">
             <Share/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+            {Posts.map((p) => (
+              <Post key={p.id} post={p} />
+            ))}
+            
         </div>
     </div>
   )
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,32 +1,34 @@
 import {MoreVert} from "@mui/icons-material"
 import "./post.css"
+import { Users } from "../../dummyData"
 
-export default function Post() {
+export default function Post({post}) {
+
   return (
     <div className='post'>
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img className='postProfileImage' src="/assets/person/1.jpeg" alt="" />
-                    <span className="postUsername">Fazlı Balkan</span>
-                    <span className="postDate">12 mins ago</span>
+                    <img className='postProfileImage' src={Users.filter(u=>u.id === post.userId)[0].profilePicture} alt="" />
+                    <span className="postUsername">{Users.filter(u=>u.id === post.userId)[0].username}</span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVert/>
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey! It's my first post.</span>
-                <img className="postImage" src="assets/post/1.jpeg" alt="" />
+                <span className="postText">{post?.desc}</span>
+                <img className="postImage" src={post.photo} alt="" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                     <img className="likeIcon" src="/assets/like.png" alt="" />
                     <img className="likeIcon" src="/assets/heart.png" alt="" />
-                    <span className="postLikeCounter">23 People Liked</span>
+                    <span className="postLikeCounter">{post.like} People Liked</span>
                 </div>
                 <div className="postBottomRight">
-                    <span className="postCommentText">9 comments</span>
+                    <span className="postCommentText">{post.comment} comments</span>
                 </div>
             </div>
         </div>
