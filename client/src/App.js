8c88c6f09a9cd0da2472d88f4c479f40e67c6b43
fazlibalkan <fazlibalkan@outlook.com>
2022-02-17 15:17:32 +0300

    ract-router-dom is initialized

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,25 @@ import Login from "./components/login/Login.jsx";
 import Register from "./components/register/Register.jsx";
 import Home from "./pages/home/Home.jsx"
 import Profile from "./pages/profile/Profile.jsx";
-//import {Person} from "@mui/icons-material"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/profile/:username" element={<Profile/>} />
+        
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
