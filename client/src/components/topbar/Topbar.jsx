ea3fb55b21dc840758da874b88a1dbffb1a156da
fazlibalkan <fazlibalkan@outlook.com>
2022-02-17 15:44:24 +0300

    link added

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,12 +1,16 @@
 import "./topbar.css"
 import { Search, Person, Chat, Notifications } from "@mui/icons-material"
+import {Link} from "react-router-dom"
 
 export default function Topbar() {
     return (
 
         <div className="topbarContainer">
             <div className="topbarLeft">
-                <span className="logo">Fazebook</span>
+                <Link to="/" style={{textDecoration:"none"}}>
+                    <span className="logo">Fazebook</span>
+                </Link>
+                
             </div>
 
             <div className="topbarCenter">
